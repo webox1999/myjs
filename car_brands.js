22ca12471e598a00a1e57d7712850191b0de3f03
webox1999 <webox1999@gmail.com>
2025-02-01 11:00:44 +0100

    Add files via upload

---
+++ car_brands.js
@@ -0,0 +1,13 @@
+const carBrands = [
+    "Acura", "Alfa Romeo", "Audi", "BMW", "Brilliance", "Cadillac", "Changan", "Chery", "Chevrolet", "Chrysler", 
+    "Citroen", "Daewoo", "Daihatsu", "Dodge", "DongFeng", "Evolute", "Exeed", "Faw", "Fiat", "Ford", 
+    "Gac", "Geely", "Genesis", "Great Wall", "Hafei", "Haima", "Haval", "Honda", "Hongqi", "Hummer", 
+    "Hyundai", "Infiniti", "Iran Khodro", "Isuzu", "Iveco", "Jac", "Jaecoo", "Jaguar", "Jeep", "Jetour", 
+    "Kia", "LDV", "Lancia", "Land Rover", "Lexus", "LiXiang", "Lifan", "Mahindra", "Mazda", "Mercedes-Benz", 
+    "Mini", "Mitsubishi", "Nissan", "Omoda", "Opel", "Peugeot", "Porsche", "Ravon", "Renault", "Rover", 
+    "Saab", "Saipa", "Saturn", "Seat", "Skoda", "Smart", "SsangYong", "Subaru", "Suzuki", "Tank", 
+    "Tesla", "Toyota", "Volkswagen", "Volvo", "Wuling", "ZX Auto", "Zeekr", "Zotye", "ВАЗ (Lada)", 
+    "МАЗ", "Москвич", "ТагАЗ"
+];
+
+export default carBrands;
